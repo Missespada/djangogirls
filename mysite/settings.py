4a20b00a26b10af92ead1a2dea9310691af85e5d
Missespada <you@example.com>
2015-07-18 17:25:49 -0700

    styled website

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,3 +102,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATIC_ROOT = ''
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = ( os.path.join('static'), )
